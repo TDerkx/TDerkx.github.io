7a059d682091de62a569cfcf62ec2465019d58c9
TDerkx <tomderkxtue@gmail.com>
2015-12-01 22:23:54 +0100

    this should do it

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
-var main = function() {
-    $(".pict").hide();
-    $('#pict1').show();
+var main = function() { // this variable contains the actions that should be taken when the site loads
+    $(".pict").hide(); // this hides all pictures when the site is first loaded
+    $('#pict1').show(); // this then ensures that the first picture is shown when the site is first loaded
     
-    $('#pic1').click(function() {
+    $('#pic1').click(function() { //these .click functions make the words in the menu on top clickable, upon which first all pictures are hidden and then the correct picture shown again.
         $('.pict').hide();
         $('#pict1').show();
         
@@ -28,4 +28,4 @@ var main = function() {
         $('#pict5').show();
     }); 
 };
-$(document).ready(main);
+$(document).ready(main); // this ensures that everything in the main variable happens from the moment the site loads.
