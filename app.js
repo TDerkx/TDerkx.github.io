9f9e4cfe6b192adc5f4df5f8aabc728e975c48e8
TDerkx <tomderkxtue@gmail.com>
2015-12-01 21:54:35 +0100

    s

--- app.js
+++ app.js
@@ -5,27 +5,27 @@ var main = function() {
     
     $('#pic1').click(function() {
         $('.pict').hide();
-                $('#pict1').show();
+        $('#pict1').show();
         
     });
     $('#pic2').click(function() {
         $('.pict').hide();
-                $('#pict2').show();
+        $('#pict2').show();
         
     });
     $('#pic3').click(function() {
         $('.pict').hide();
-                $('#pict3').show();
+        $('#pict3').show();
         
     });
     $('#pic4').click(function() {
         $('.pict').hide();
-                $('#pict4').show();
+        $('#pict4').show();
         
     });
     $('#pic5').click(function() {
         $('.pict').hide();
-                $('#pict5').show();
+        $('#pict5').show();
     }); 
 };
 $(document).ready(main);
