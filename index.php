d3150fe3edaa9e1adc1a7c4831784d385e72272a
TDerkx <tomderkxtue@gmail.com>
2015-12-01 17:30:24 +0100

    test

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php include 'header2.php'; ?>
+	<div id="page" style="width: 1000px; height: 800px"> <!-- 931/628 -->
+		<span style="visibility:hidden" >...weet niet hoe ik dit moet oplossen, dus dan maar zo </span>
+		<?php
+		include 'header.html'
+		?>
+
+				   
+    <div id="wrapper"> 
+        
+        <div id="content">
+        
+            <div id="path">
+                U bevindt zich hier: <a href="index.php">Home</a> 
+                &raquo <u>Introductiepagina</u>
+
+            </div>
+    
+            <div id="main">
+
+                <h1>
+					Welkom op huisjestehuur.nl !</h1>
+    
+                <p>[informatie] </p>
+                    
+                <br />
+                
+                
+                <h1>
+
+				[Kop1]</h1>
+    
+                <p>[informatie] </p>
+				<h1>
+		
+						[Kop2]</h1>
+                
+                <div class="img_right">
+                    </div>
+
+                <p>
+                    [informatie] 
+                </p>
+                
+    
+				<p>
+                    &nbsp;</p>
+				<p>
+        <p>          
+				        
+            </div>
+
+        </div>
+        
+						<?php
+						include 'navig.html'
+						?>
+
+						<?php
+						include 'nieuws.html'
+						?>
+            
+						<?php
+						include 'links.html'
+						?>
+						
+						<!-- conflict met andere scripts/harder geluid plotseling 
+						<?php
+						include 'audio.html'
+						?>
+						-->
+        </div>
+
+    </div>
+    
+            <?php
+						include 'footer.html'
+						?>
+
+</div>
+
+<!-- einde index etc. -->
+    
+    <div style="clear:both; display:block; height:100px"></div>
+</body> 
+</html>
