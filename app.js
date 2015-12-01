8971f55ab97c9e6387b44b08f948845b75786f74
TDerkx <tomderkxtue@gmail.com>
2015-12-01 21:30:40 +0100

    Maybe

---
+++ app.js
@@ -0,0 +1,31 @@
+
+var main = function() {
+    $(".pict").hide();
+    $('#pict1').show();
+    
+    $('#pic1').click(function() {
+        $('.pict').hide();
+                $('#pict1').show();
+        
+    });
+    $('#pic2').click(function() {
+        $('.pict').hide();
+                $('#pict2').show();
+        
+    });
+    $('#pic3').click(function() {
+        $('.pict').hide();
+                $('#pict3').show();
+        
+    });
+    $('#pic4').click(function() {
+        $('.pict').hide();
+                $('#pict4').show();
+        
+    });
+    $('#pic5').click(function() {
+        $('.pict').hide();
+                $('#pict5').show();
+    }); 
+};
+$(document).ready(main);
