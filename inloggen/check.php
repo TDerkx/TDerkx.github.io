2f94193d696a105ea0d4df950d05733a8a95ec38
TDerkx <tomderkxtue@gmail.com>
2015-12-01 17:41:14 +0100

    check

--- inloggen/check.php
+++
@@ -1,5 +0,0 @@
-<?php 
-if( isset($_GET['text1']) && $_GET['text1'] !== '' 
-if( isset($_GET['text2']) && $_GET['text2'] !== '' 
-if( isset($_GET['text3']) && $_GET['text3'] !== '' 
-?>
--- inloggen/conf.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$mysql = mysql_connect("localhost","boesboes","geheim") or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
-mysql_select_db("casus_vakantiehuisjes",$mysql) or die("Fout: Het openen van de database is mislukt!");
-?>
